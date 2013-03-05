7e0e92827e1e5e01a95d90bcc98896b3d16efe9f
khoramshahi <m80.khoramshahi@gmail.com>
2013-03-04 20:22:27 -0500

    fixing nonlinear controller

--- Variable_DutyFactor/FourLeg_VDF/SimParam_4VDF.m
+++ Variable_DutyFactor/FourLeg_VDF/SimParam_4VDF.m
@@ -4,14 +4,14 @@ clear all
 
 %%%%%%%%%%%%%
 mass = 0.1;
-m_toe = 0.0;
+m_toe = 0.0001;
 leg_length = 0.0249;
 
 frame_width  = 0.125; 
 frame_length = 0.25;
 frame_height = 0.02;
 
-CG_pos =[frame_length*0.1 0 0];
+CG_pos =[frame_length*0.0 0 0];
 
 TR_FR = [ frame_length/2 0  frame_width/2 ];
 TR_FL = [ frame_length/2 0 -frame_width/2 ];
@@ -35,6 +35,7 @@ freq = 60;
 
 y_0 = leg_length;
 y_0 = 0.01;
+dy_0 = 0.3;
 
 r1 = 0.02;
 r2 = r1/4;
@@ -54,7 +55,9 @@ tau = 0.001;
 alpha = 30;
 t_fil =0.05;
 %%
-K_p = -50;
+K_p = -5;
 K_i = 0;
 K_d = 0;
+
+K_vir_spring = 5;
 
