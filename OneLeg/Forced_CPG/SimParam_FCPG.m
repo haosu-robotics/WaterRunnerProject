41b9ab85eed168b43430e464e4c4c6b271d6b194
khoramshahi <m80.khoramshahi@gmail.com>
2013-03-07 17:11:51 -0500

    Forced_CPG added

---
+++ OneLeg/Forced_CPG/SimParam_FCPG.m
@@ -0,0 +1,31 @@
+clc
+close all
+clear all
+
+
+mass = 0.05;
+leg_length = 0.06;
+
+amp = leg_length;
+freq = 60;
+RL_lag = pi;
+
+y_0 = leg_length;
+
+r1 = 0.02;
+r2 = r1/4;
+S1 = pi*r1^2;
+S2 = pi*r2^2;
+
+density = 1000;
+g = 9.81;
+C_d = 0.703;
+
+b_water = 0.5*C_d*S1*density;
+k_water = C_d*S1*density*g;
+FRatio = 1/16;
+
+
+tau = 0.001;
+alpha = 30;
+t_fil = 0.2;
