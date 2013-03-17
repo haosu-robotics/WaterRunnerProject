983b54bdfe67ea6c2f1300abba5ec027711f7203
Nitish Thatte <nitishsthatte@gmail.com>
2013-03-17 00:38:43 -0400

    Tuned Gains. Added simulated waves

--- FourLegs/Forced/SimParam_4VDF.m
+++ FourLegs/Forced/SimParam_4VDF.m
@@ -23,10 +23,10 @@ Iz = 1/12*mass*( frame_length^2 + frame_height^2 );
 frame_inertia = diag([Ix,Iy,Iz]);
 
 mass_matrix = [mass 0 0; 0 Ix 0 ; 0 0 Iz];
-Lx = frame_length/2*[1 1 -1 -1]';
-Lxmat = frame_length/2*[1 0 0 0 ; 0 1 0 0 ; 0 0 -1 0 ; 0 0 0 -1];
-Lz = frame_width/2*[1 -1 -1 1]';
-Lzmat = frame_width/2*[1 0 0 0 ; 0 -1 0 0 ; 0 0 -1 0 ; 0 0 0 1];
+Lx = frame_length/2*[-1 -1 1 1]';
+Lxmat = frame_length/2*[-1 0 0 0 ; 0 -1 0 0 ; 0 0 1 0 ; 0 0 0 1];
+Lz = frame_width/2*[-1 1 1 -1]';
+Lzmat = frame_width/2*[-1 0 0 0 ; 0 1 0 0 ; 0 0 1 0 ; 0 0 0 -1];
 
 %%%%% Water Model params %%%%%
 
@@ -38,7 +38,7 @@ omega_1 = freq*ones(2,1);
 
 y_0 = leg_length/2;
 
-r1 = 0.02;
+r1 = 0.020;
 r2 = r1/4;
 S1 = pi*r1^2*0.9185;
 S2 = pi*r2^2*0.9185;
@@ -51,10 +51,14 @@ b_water = 0.5*C_d*S1*density;
 k_water = C_d*S1*density*g;
 Fratio = 1/16;
 
+wave = .025;
+wavefreq = .5;
+
 %%% PID gains %%%%
-K_p = 500;
-K_i = 1000;
-K_d = 0;
+K_p = [500 0 0 ; 0 1000 0; 0 0 1000];
+K_i = [1000 0 0 ; 0 10000 0; 0  0 10000];
+K_d = [50 0 0; 0 600 0; 0 0 600];
+Filter_Coef = 1000;
 
 %%%%% CPG params %%%%%
 FR_FL_lag = pi;     % l1
@@ -62,7 +66,7 @@ HR_HL_lag = -pi;    % l2
 FR_HR_lag = pi;     % l3
 FL_HL_lag = -pi;    % l4
 
-c_gain = 30;
+c_gain = 100;
 cw_FR_FL = c_gain;
 cw_HR_HL = c_gain;
 cw_FR_HR = c_gain;
