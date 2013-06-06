10765d1cd532e72ecc36b80caf6e2461265a6e86
Nitish Thatte <nitishsthatte@gmail.com>
2013-06-06 00:27:32 -0400

    Near final version

--- DynamicWalking/figures/results.py
+++ DynamicWalking/figures/results.py
@@ -10,7 +10,7 @@ pgf_with_custom_preamble = {
     "pgf.texsystem": "xelatex",
     "font.family": "sans-serif", # use serif/main font for text elements
     "text.usetex": True,    # use inline math for ticks
-    "pgf.rcfonts": True,   # don't setup fonts from rc parameters
+    "pgf.rcfonts": True,   
     "pgf.preamble": [
          r"\usepackage{amsmath}",
          r"\usepackage{fontspec}",
@@ -23,7 +23,7 @@ pgf_with_custom_preamble = {
 }
 mpl.rcParams.update(pgf_with_custom_preamble)
 
-torques = np.array([2.5e-3, 3e-3, 3.25e-3, 3.5e-3])
+torques = np.array([3e-3, 3.5e-3, 4e-3, 4.5e-3, 5e-3])
 
 data = sio.loadmat('data.mat')
 closedResponse1= np.array(data['closedResponse1'][0])
@@ -40,36 +40,36 @@ ax1.spines['top'].set_visible(False)
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
 
-for i in np.arange(4):
+for i in np.arange(torques.shape[0]):
     plt.plot(closedResponse1[i][:,0],closedResponse1[i][:,1], linewidth=2, label = str(torques[i]*1e3) + ' mN-m')
 
 #set title
-plt.title('Response with Heuristic')
+plt.title('Roll Angle Response')
 curpos = ax1.title.get_position()
 ax1.title.set_position((curpos[0], curpos[1] + 0.025))
 
 #set and align axis labels
-plt.ylabel('roll angle [degrees]')
-labelx = -0.05
+plt.ylabel('roll angle [deg]')
+labelx = -0.06
 ax1.yaxis.set_label_coords(labelx, 0.5)
-plt.legend(bbox_to_anchor=(1.05, -0.55),loc = 3,borderaxespad=0.,frameon=False)
-plt.axis([0, 10, -20, 20])
+plt.legend(bbox_to_anchor=(1.05, -0.75),loc = 3,borderaxespad=0.,frameon=False)
+plt.axis([0, 8, -30, 10])
 
 #phase relationships
 ax2 = plt.subplot(212)
-for i in np.arange(4):
+for i in np.arange(torques.shape[0]):
     plt.plot(phases1[i][:,0],phases1[i][:,1], linewidth=2, label = str(torques[i]*1e3) + ' mN-m')
 
 #set title
-plt.title('Leg Phase Relationship')
+plt.title('Leg Phase Difference')
 curpos = ax2.title.get_position()
 ax2.title.set_position((curpos[0], curpos[1] + 0.025))
 
 #set and align axis labels
 plt.xlabel('time [s]')
-plt.ylabel('phase [radians]')
+plt.ylabel('phase difference [rad]')
 ax2.yaxis.set_label_coords(labelx, 0.5)
-plt.axis([0, 10, 0, 2*np.pi])
+plt.axis([0, 8, 0, 2*np.pi])
 ax2.spines['right'].set_visible(False)
 ax2.spines['top'].set_visible(False)
 ax2.xaxis.set_ticks_position('bottom')
@@ -94,20 +94,19 @@ ax1.spines['right'].set_visible(False)
 ax1.spines['top'].set_visible(False)
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
-for i in np.arange(4):
+for i in np.arange(torques.shape[0]):
     plt.plot(closedResponse2[i][:,0],closedResponse2[i][:,1], linewidth=2, label = str(torques[i]*1e3) + ' mN-m')
 
 #set title
-plt.title('Response without Heuristic')
+plt.title('Roll Angle Response')
 curpos = ax1.title.get_position()
 ax1.title.set_position((curpos[0], curpos[1] + 0.025))
 
 #set and align axis labels
-plt.ylabel('roll angle [degrees]')
-labelx = -0.05
+plt.ylabel('roll angle [deg]')
 ax1.yaxis.set_label_coords(labelx, 0.5)
-plt.legend(bbox_to_anchor=(1.05, -0.55),loc = 3,borderaxespad=0.,frameon=False)
-plt.axis([0, 10, -20, 20])
+plt.legend(bbox_to_anchor=(1.05, -0.75),loc = 3,borderaxespad=0.,frameon=False)
+plt.axis([0, 8, -30, 10])
 
 #phase relationships
 ax2 = plt.subplot(212)
@@ -115,19 +114,19 @@ ax2.spines['right'].set_visible(False)
 ax2.spines['top'].set_visible(False)
 ax2.xaxis.set_ticks_position('bottom')
 ax2.yaxis.set_ticks_position('left')
-for i in np.arange(4):
+for i in np.arange(torques.shape[0]):
     plt.plot(phases2[i][:,0],phases2[i][:,1], linewidth=2, label = str(torques[i]*1e3) + ' mN-m')
 
 #set title
-plt.title('Leg Phase Relationship')
+plt.title('Leg Phase Difference')
 curpos = ax2.title.get_position()
 ax2.title.set_position((curpos[0], curpos[1] + 0.025))
 
 #set and align axis labels
 plt.xlabel('time [s]')
-plt.ylabel('phase [radians]')
+plt.ylabel('phase difference [rad]', multialignment = 'center')
 ax2.yaxis.set_label_coords(labelx, 0.5)
-plt.axis([0, 10, 0, 2*np.pi])
+plt.axis([0, 8, 0, 2*np.pi])
 ax2.spines['right'].set_visible(False)
 ax2.spines['top'].set_visible(False)
 ax2.xaxis.set_ticks_position('bottom')
